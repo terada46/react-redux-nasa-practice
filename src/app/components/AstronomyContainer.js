13f33c5efcc36cc82511ae92ff037e24f214ab05
terada46 <mizukiloveu@gmail.com>
2018-03-18 16:07:34 +0800

    initial project.

---
+++ src/app/components/AstronomyContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import AstronomyCard from './AstronomyCard';
+import { connect } from 'react-redux';
+import fetchData from '../../actions/fetch_data';
+
+class AstronomyContainer extends Component {
+
+	componentDidMount() {
+		this.props.fetchData();
+	}
+
+	render() {
+		return (
+			<AstronomyCard data={this.props.astronomy} />
+		)
+	}
+} 
+
+// connect react with redux
+// @params mapStateToProps
+// @params mapDispatchToProps
+function mapStateToProps(state) {
+	return { astronomy: state.astronomy };
+}
+
+export default connect(mapStateToProps, { fetchData })(AstronomyContainer);
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'; // import the main react dependency
+import ReactDOM from 'react-dom'; // import reactDOM
+import { Provider } from 'react-redux'; // Glue for react and redux
+import Store from './Store'; //redux state store
+import App from './app/App.jsx'; // import the main app component
+
+import './assets/stylesheets/styles.scss';
+
+ReactDOM.render(
+	<Provider store={Store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root') // and mount it to our #root element
+); 
